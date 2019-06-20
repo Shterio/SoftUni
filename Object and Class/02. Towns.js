48a86cca4bbdc8d6e99a21519c9e956a6b487422
Shterio <Shterio@users.noreply.github.com>
2019-06-20 12:27:23 +0200

    new

--- Object and Class/02. Towns.js
+++ Object and Class/02. Towns.js
@@ -9,10 +9,17 @@ towns = (array) => {
     let citys = [];
     for (let arrayElement of array) {
         let [town,latitude,longitude] = arrayElement.split(' | ');
+        latitude = Number(latitude).toFixed(2);
+        longitude = Number(longitude).toFixed(2);
         let city = new Town(town,latitude,longitude);
         citys.push(city);
     }
-    console.log(citys);
+    for (let i = 0; i < citys.length; i++) {
+        console.log(`{ town: '${citys[i].town}', latitude: '${citys[i].latatude}', longitude: '${citys[i].longitude}' }`);
+
+    }
+
+
 };
 towns(['Sofia | 42.696552 | 23.32601',
     'Beijing | 39.913818 | 116.363625']);

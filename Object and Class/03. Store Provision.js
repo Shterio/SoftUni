f649da01831cfba44f2fdef150c423bb28cc6f57
Shterio <Shterio@users.noreply.github.com>
2019-06-20 13:37:24 +0200

    new

--- Object and Class/03. Store Provision.js
+++ Object and Class/03. Store Provision.js
@@ -1,12 +1,21 @@
-store = (stock,delivery) => {
+store = (stock, delivery) => {
     let products = {};
 
-    for (let i = 0; i < stock.length; i+=2) {
+    for (let i = 0; i < stock.length; i += 2) {
 
-        products[stock[i]]= Number(stock[i+1]);
+        products[stock[i]] = Number(stock[i + 1])
+    }
+    for (let j = 0; j < delivery.length; j += 2) {
+        if (products.hasOwnProperty(delivery[j])) {
+            products[delivery[j]] += Number(delivery[j + 1]);
+        } else {
+            products[delivery[j]] = Number(delivery[j + 1])
+        }
 
     }
-    console.log(products);
+    for (let key in products) {
+     console.log(`${key} -> ${products[key]}`);
+    }
 };
 store([
         'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'

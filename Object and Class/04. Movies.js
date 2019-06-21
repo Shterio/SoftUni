82ee7590b91064b536bf88d27ce7bd92309ddf7e
Shterio <Shterio@users.noreply.github.com>
2019-06-22 00:15:12 +0200

    new

--- Object and Class/04. Movies.js
+++ Object and Class/04. Movies.js
@@ -1,13 +1,14 @@
 movies = (input) => {
-    let movies = {};
+    let movies = [];
 
     for (let inputElement of input) {
         if (inputElement.includes('addMovie')) {
-            let element = inputElement.split('addMovie ');
-            if (!movies.hasOwnProperty(element[1])){
-                movies[element] = []
-            }
+            let token = inputElement.split('addMovie ');
+            addMovie(token[1])
         }
+    }
+    function addMovie (movieName){
+        return movies.push({name: movieName});
     }
     console.log(movies);
 

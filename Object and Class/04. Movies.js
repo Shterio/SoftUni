bdeb92d5df6bdb78015f9e3da3e473231efc888d
Shterio <Shterio@users.noreply.github.com>
2019-06-21 16:32:26 +0200

    new

---
+++ Object and Class/04. Movies.js
@@ -0,0 +1,19 @@
+movies = (input) => {
+    class Movie {
+        constructor(title,director,date){
+            this.title = title;
+            this.director = director;
+            this.date = date;
+        }
+    }
+};
+movies([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'
+]);

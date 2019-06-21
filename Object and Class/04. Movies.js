8767fe2491fc0395ba412f2bb9393603c360d037
Shterio <Shterio@users.noreply.github.com>
2019-06-22 00:20:54 +0200

    new

--- Object and Class/04. Movies.js
+++ Object and Class/04. Movies.js
@@ -5,10 +5,18 @@ movies = (input) => {
         if (inputElement.includes('addMovie')) {
             let token = inputElement.split('addMovie ');
             addMovie(token[1])
+        } else if (inputElement.includes('directedBy')){
+            let [movieName, director] = inputElement.split(' directedBy ');
         }
     }
     function addMovie (movieName){
         return movies.push({name: movieName});
+    }
+    function addDirector (movieName, director){
+        let foundMovie = movies.find(m => m.name === movieName);
+        if (foundMovie){
+            movies.movieName = director;
+        }
     }
     console.log(movies);
 

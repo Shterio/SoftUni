77f44f212206ba4d15edb6f01c953e0f0e6ba776
Shterio <Shterio@users.noreply.github.com>
2019-06-24 19:57:23 +0200

    n

--- Object and Class/03. School Register.js
+++ Object and Class/03. School Register.js
@@ -1,52 +1,15 @@
-school =(array) => {
-    let grade9 = [[],[]];
-    let grade10 = [[],[]];
-    let grade11 = [[],[]];
-    let grade12 = [[],[]];
-    let avarege = [];
+school = (array) => {
+    let obj = new Map();
 
-
-    for (let arrayElement of array) {
-        let [name,grade,score]= arrayElement.split(', ');
-        name = name.split(': ')[1];
-        grade = Number(grade.split(': ')[1]);
-        score = Number(score.split(': ')[1]);
-
-        if (grade === 8){
-            if (score >= 3){
-                grade9[0].push(name);
-                grade9[1].push(score);
-            }
-        }
-        if (grade === 9){
-            if (score >= 3){
-               grade10[0].push(name);
-               grade10[1].push(score);
-            }
-        }
-        if (grade === 10){
-            if (score >= 3){
-                grade11[0].push(name);
-                grade11[1].push(score);
-            }
-        }
-        if (grade === 11){
-            if (score >= 3){
-                grade12[0].push(name);
-                grade12[1].push(score);
-            }
-        }
+    for (let argument of array) {
+        let line = argument.split(': ').join(' ').split(', ');
+        let name = line[0].split(' ')[2];
+        let score = Number(line[1].split(' ')[1]) + 1;
+        console.log(score);
+        console.log(line);
     }
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-
-    function score(input) {
-        input[1] = input[1].reduce(reducer) / input[1].length;
-    }
-
-
-console.log(grade10);
 };
-school([ 'Student name: Mark, Grade: 8, Graduated with an average score: 4.75',
+school(['Student name: Mark, Grade: 8, Graduated with an average score: 4.75',
     'Student name: Ethan, Grade: 9, Graduated with an average score: 5.66',
     'Student name: George, Grade: 8, Graduated with an average score: 2.83',
     'Student name: Steven, Grade: 10, Graduated with an average score: 4.20',
@@ -57,5 +20,5 @@ school([ 'Student name: Mark, Grade: 8, Graduated with an average score: 4.75',
     'Student name: Bill, Grade: 9, Graduated with an average score: 6.00',
     'Student name: Philip, Grade: 10, Graduated with an average score: 5.05',
     'Student name: Peter, Grade: 11, Graduated with an average score: 4.88',
-    'Student name: Gavin, Grade: 10, Graduated with an average score: 4.00' ]
+    'Student name: Gavin, Grade: 10, Graduated with an average score: 4.00']
 );

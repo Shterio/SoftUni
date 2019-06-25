e129d7434f02da7208b0890d85d8d009c4206104
Shterio <Shterio@users.noreply.github.com>
2019-06-25 20:37:45 +0200

    n

--- Object and Class/01. Employees.js
+++ Object and Class/01. Employees.js
@@ -1,20 +1,25 @@
 employees = (array) => {
     class Employer {
-        constructor (name,number){
+        constructor(name, number) {
             this.name = name;
             this.number = number;
         }
     }
 
-    let staff = [];
+    let staff = new Map();
+    //let staff = [];
     for (let i = 0; i < array.length; i++) {
         let name = array[i];
         let number = array[i].length;
-        let person = new Employer(name,number);
-        staff.push(person);
+        //let person = new Employer(name, number);
+        //staff.push(person)
+        staff.set(name, number);
 
     }
-    staff.forEach(i => console.log(`Name: ${i.name} -- Personal Number: ${i.number}`));
+
+    let staffs = Array.from(staff);
+    //staff.forEach(i => console.log(`Name: ${i.name} -- Personal Number: ${i.number}`));
+    staffs.forEach(i => console.log(`Name: ${i[0]} -- Personal Number: ${i[1]}`));
 };
 employees([
     'Silas Butler',

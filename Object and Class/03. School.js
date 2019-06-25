84730c8c21963f9ab57eedfea8324cae93a46cce
Shterio <Shterio@users.noreply.github.com>
2019-06-25 20:40:26 +0200

    n

--- Object and Class/03. School.js
+++ Object and Class/03. School.js
@@ -12,3 +12,21 @@ function solve(arr) {
 
         if (averageScore < 3) {
 
+        } else {
+            if (!studentObj.hasOwnProperty(grade)) {
+
+                studentObj[grade] = [{'name': name, 'averageScore': averageScore}];
+            } else {
+
+                studentObj[grade].push({'name': name, 'averageScore': averageScore});
+            }
+        }
+    }
+
+    let objEntries = Object.entries(studentObj)
+        .sort((a, b) => a[0] - b[0]);
+    let names = [];
+    let sum = 0;
+    let average = 1;
+    let counter = 0;
+

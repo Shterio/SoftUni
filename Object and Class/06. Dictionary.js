f940b7f1feab3884c8e9f228d69da76bd0b7d4b5
Shterio <Shterio@users.noreply.github.com>
2019-06-27 21:32:06 +0200

    n

--- Object and Class/06. Dictionary.js
+++ Object and Class/06. Dictionary.js
@@ -2,3 +2,8 @@ solve = (input) => {
     let dictionary = {};
     let obj = input.map(i => JSON.parse(i));
 
+    obj.forEach(i => {
+        let tuple = Object.entries(i);
+        let term = tuple[0][0];
+        dictionary[term] = tuple[0][1];
+    });

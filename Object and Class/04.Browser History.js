85e4c2683c64049a120c2ad6d16213cf036559ed
Shterio <Shterio@users.noreply.github.com>
2019-06-28 21:52:01 +0200

    n

--- Object and Class/04.Browser History.js
+++ Object and Class/04.Browser History.js
@@ -1,7 +1,21 @@
-browser =(input,array) => {
+browser = (input, array) => {
+    let history = input;
+    for (let arrayElement of array) {
+        let [command, name] = arrayElement.split(' ');
+        if (command === 'Close') {
+            let index = history["Open Tabs"].indexOf(name);
+            if (index !== -1) {
+                history["Open Tabs"].splice(index, 1);
+            }
+            console.log(history["Open Tabs"]);
+        } else if (command === 'Open') {
 
+        }
+    }
 };
-browser({"Browser Name":"Google Chrome","Open Tabs":["Facebook","YouTube","Google Translate"],
-        "Recently Closed":["Yahoo","Gmail"],
-        "Browser Logs":["Open YouTube","Open Yahoo","Open Google Translate","Close Yahoo","Open Gmail","Close Gmail","Open Facebook"]},
+browser({
+        "Browser Name": "Google Chrome", "Open Tabs": ["Facebook", "YouTube", "Google Translate"],
+        "Recently Closed": ["Yahoo", "Gmail"],
+        "Browser Logs": ["Open YouTube", "Open Yahoo", "Open Google Translate", "Close Yahoo", "Open Gmail", "Close Gmail", "Open Facebook"]
+    },
     ["Close Facebook", "Open StackOverFlow", "Open Google"]);

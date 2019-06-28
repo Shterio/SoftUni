f708e4b9fc281acf68c73f30465649e455b49225
Shterio <Shterio@users.noreply.github.com>
2019-06-28 21:25:38 +0200

    n

--- Object and Class/04. Browser.js
+++ Object and Class/04. Browser.js
@@ -18,3 +18,13 @@ function browserHistory(input, actions) {
             output['Browser Logs'] = [];
         }
     }
+    let result = Object.entries(output);
+
+    result.forEach(element => {
+        if (element[0] === 'Browser Name') {
+            console.log(element[1]);
+        } else {
+            console.log(`${element[0]}: ${element[1].join(', ')}`);
+        }
+    });
+}

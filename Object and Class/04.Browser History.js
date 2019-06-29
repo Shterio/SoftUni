89477bf8f038ebb947562e4b5715222d76711280
Shterio <Shterio@users.noreply.github.com>
2019-06-29 08:31:08 +0200

    n

--- Object and Class/04.Browser History.js
+++ Object and Class/04.Browser History.js
@@ -34,3 +34,10 @@ browser({
         "Browser Logs": ["Open YouTube", "Open Yahoo", "Open Google Translate", "Close Yahoo", "Open Gmail", "Close Gmail", "Open Facebook"]
     },
     ["Close Facebook", "Open StackOverFlow", "Open Google"]);
+browser({
+        "Browser Name": "Mozilla Firefox",
+        "Open Tabs": ["YouTube"],
+        "Recently Closed": ["Gmail", "Dropbox"],
+        "Browser Logs": ["Open Gmail", "Close Gmail", "Open Dropbox", "Open YouTube", "Close Dropbox"]
+    },
+    ["Open Wikipedia", "Clear History and Cache", "Open Twitter"]);

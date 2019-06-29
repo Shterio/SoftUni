f7aba7ffdeaf6835ebb618f7d7e2ffe131657682
Shterio <Shterio@users.noreply.github.com>
2019-06-29 08:29:25 +0200

    n

--- Object and Class/04.Browser History.js
+++ Object and Class/04.Browser History.js
@@ -5,17 +5,18 @@ browser = (input, array) => {
         if (command === 'Close') {
             let index = history["Open Tabs"].indexOf(name);
             if (index !== -1) {
-                history["Open Tabs"].splice(index, 1);
+                let element = history["Open Tabs"].splice(index, 1);
+                history["Recently Closed"].push(element.join());
+                history["Browser Logs"].push(arrayElement);
             }
-            console.log(history["Open Tabs"]);
-        } else if (command === 'Open') {
 
+        } else if (command === 'Open') {
+            history["Open Tabs"].push(name);
+            history["Browser Logs"].push(arrayElement)
+        } else if (command === 'Clear') {
+            history["Open Tabs"] = [];
+            history["Recently Closed"] = [];
+            history["Browser Logs"] = [];
         }
+
     }
-};
-browser({
-        "Browser Name": "Google Chrome", "Open Tabs": ["Facebook", "YouTube", "Google Translate"],
-        "Recently Closed": ["Yahoo", "Gmail"],
-        "Browser Logs": ["Open YouTube", "Open Yahoo", "Open Google Translate", "Close Yahoo", "Open Gmail", "Close Gmail", "Open Facebook"]
-    },
-    ["Close Facebook", "Open StackOverFlow", "Open Google"]);

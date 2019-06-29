02fe18982987df0b8df5305e794afb999b491620
Shterio <Shterio@users.noreply.github.com>
2019-06-29 08:30:44 +0200

    n

--- Object and Class/04.Browser History.js
+++ Object and Class/04.Browser History.js
@@ -28,3 +28,9 @@ browser = (input, array) => {
     console.log(`Browser Logs: ${browsers[3][1].join(', ')}`);
 
 };
+browser({
+        "Browser Name": "Google Chrome", "Open Tabs": ["Facebook", "YouTube", "Google Translate"],
+        "Recently Closed": ["Yahoo", "Gmail"],
+        "Browser Logs": ["Open YouTube", "Open Yahoo", "Open Google Translate", "Close Yahoo", "Open Gmail", "Close Gmail", "Open Facebook"]
+    },
+    ["Close Facebook", "Open StackOverFlow", "Open Google"]);

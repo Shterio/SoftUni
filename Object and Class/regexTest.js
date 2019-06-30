ddb27df52d92001db4c58247794352c60b7b50df
Shterio <Shterio@users.noreply.github.com>
2019-06-30 14:28:15 +0200

    n

--- Object and Class/regexTest.js
+++ Object and Class/regexTest.js
@@ -7,3 +7,12 @@ regex = (input) => {
             this.lon = lon;
         }
     }
+    let arr = [];
+
+    for (let inputElement of input) {
+        let [town, lat, lon] = inputElement.split('|').filter(Boolean).map(i => i.trim());
+        let city = new City(town, lat, lon)
+        arr.push(city)
+    }
+    console.log(arr.map(i => JSON.stringify(i)));
+};

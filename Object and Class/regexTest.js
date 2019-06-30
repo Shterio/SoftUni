5e7c6509c62e1ff2bb5dbbc1e15ee51f0915b357
Shterio <Shterio@users.noreply.github.com>
2019-06-30 14:27:53 +0200

    n

--- Object and Class/regexTest.js
+++ Object and Class/regexTest.js
@@ -1,1 +1,9 @@
 regex = (input) => {
+    let [town, lat, lon] =input.shift().split('|').filter(Boolean).map(i => i.trim());
+    class City {
+        constructor(town,lat, lon){
+            this.town = town;
+            this.lat = lat;
+            this.lon = lon;
+        }
+    }

ef83f56c01368dad93a5378cb162b6f51fe46df9
Shterio <Shterio@users.noreply.github.com>
2019-07-01 21:17:55 +0200

    new

--- Object and Class/04. Party Time.js
+++ Object and Class/04. Party Time.js
@@ -7,4 +7,19 @@ party = (input) => {    let output = new Map();
         if (currentOperation === 'PARTY') {
             guestComming = true;
         } else {
+            if (guestComming === false) {
+                if (!isNaN(currentOperation[0])) {
+                    output.get('VIP').push(currentOperation);
+                } else {
+                    output.get('regular').push(currentOperation);
+                }
+            } else {
+                if (!isNaN(currentOperation[0])) {
+                    output.get('VIP').splice(output.get('VIP').indexOf(currentOperation), 1);
+                } else {
+                    output.get('regular').splice(output.get('regular').indexOf(currentOperation), 1);
+                }
+            }
+        }
+    }
 

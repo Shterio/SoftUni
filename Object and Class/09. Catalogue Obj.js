f083ea88dc0d70d995dd2a9ee0dc327d0dedf094
Shterio <Shterio@users.noreply.github.com>
2019-07-02 20:58:07 +0200

    new

--- Object and Class/09. Catalogue Obj.js
+++ Object and Class/09. Catalogue Obj.js
@@ -4,3 +4,7 @@ catalogue = (input) => {
     for (let element of input) {
         let initial = element[0];
         let [product, price] = element.split(' : ');
+        price = Number(price);
+        if (!obj.hasOwnProperty(initial)) {
+            obj[initial] = [];
+        }

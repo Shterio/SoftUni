6871287e68f03e4b2317d83631ec6b8cf2472e70
Shterio <Shterio@users.noreply.github.com>
2019-07-03 20:41:12 +0200

    new

--- MID EXAM Demo/01.Problem.js
+++ MID EXAM Demo/01.Problem.js
@@ -2,3 +2,8 @@ party = (input) => {
     let size = Number(input.shift());
     let days = Number(input.shift());
 
+    let earnsCoins = 0;
+    for (let i = 1; i <= days; i++) {
+        if (i % 10 === 0) {
+            size -= 2;
+        }

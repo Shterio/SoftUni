ba4c426e0aef11050849d52b55fd08f3b56108e2
Shterio <Shterio@users.noreply.github.com>
2019-07-05 22:35:44 +0200

    new

--- MID EXAM Demo/02.Problem.js
+++ MID EXAM Demo/02.Problem.js
@@ -1,3 +1,9 @@
 eastern = (array) => {
     let gifts = array.shift().split(' ');
 
+    for (let arrayElement of array) {
+        if (arrayElement === 'No Money') {
+            let filteredGifts = gifts.filter(i => i !== 'None');
+            console.log(filteredGifts.join(' '));
+            break;
+        }

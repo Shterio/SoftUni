8ae89b873399139388248fa62761ce1390a1a54e
Shterio <Shterio@users.noreply.github.com>
2019-07-06 16:59:34 +0200

    new

--- MID EXAM Demo/03. Problem.js
+++ MID EXAM Demo/03. Problem.js
@@ -5,4 +5,8 @@ lastStop = (array) => {
         if (arrayElement === 'END') {
             console.log(numbers.join(' '));
             break;
-        }
+        }else {
+            let [command, firstNum, secondNum] = arrayElement.split(' ');
+            firstNum = Number(firstNum);
+            secondNum = Number(secondNum);
+

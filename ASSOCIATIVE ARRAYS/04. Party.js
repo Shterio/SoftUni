57b47c74cd65afbae576e7d467bf93d72eaaed28
Shterio <Shterio@users.noreply.github.com>
2019-07-07 02:28:13 +0200

    new

--- ASSOCIATIVE ARRAYS/04. Party.js
+++ ASSOCIATIVE ARRAYS/04. Party.js
@@ -3,3 +3,10 @@ party = (input) => {
     let result = {
         in: []
     };
+    let isStarted = false;
+    input.forEach(elem => {
+        if (elem === "PARTY") {
+            isStarted = true;
+        } else {
+            void (0);
+        }

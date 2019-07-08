30195e100c80c24be3c73906ea86a122b7d9d7bd
Shterio <Shterio@users.noreply.github.com>
2019-07-08 23:01:33 +0200

    new

--- ASSOCIATIVE ARRAYS/04. Book Shelf.js
+++ ASSOCIATIVE ARRAYS/04. Book Shelf.js
@@ -30,3 +30,15 @@ book = (array) => {
         }
     }
 };
+book(['1 -> history', '1 -> action',
+    'Death in Time: Criss Bell, mystery',
+    '2 -> mystery', '3 -> sci-fi',
+    'Child of Silver: Bruce Rich, mystery',
+    'Hurting Secrets: Dustin Bolt, action',
+    'Lions and Rats: Gabe Roads, history',
+    '2 -> romance',
+    'Effect of the Void: Shay B, romance',
+    'Name of Earth: Jo Bell, sci-fi',
+    'Losing Dreams: Gail Starr, sci-fi',
+    'Future of Dawn: Aiden Rose, sci-fi',
+    'Pilots of Stone: Brook Jay, history']);

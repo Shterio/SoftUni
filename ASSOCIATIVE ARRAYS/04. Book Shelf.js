fbba7dc55ea5e13b64be8d1cbdecca9614690ccd
Shterio <Shterio@users.noreply.github.com>
2019-07-08 22:59:29 +0200

    new

--- ASSOCIATIVE ARRAYS/04. Book Shelf.js
+++ ASSOCIATIVE ARRAYS/04. Book Shelf.js
@@ -1,4 +1,12 @@
 book = (array) => {
     let books = {};
 
-}
+    array.forEach(element => {
+        if (element.includes(' -> ')) {
+            let [id, genre] = element.split(' -> ');
+            id = Number(id);
+            if (!(id in books)) {
+                books[id] = [];
+                books[id].push(genre, [])
+            }
+        }

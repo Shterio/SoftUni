556978abe1fc756a845cb5919722e30c9c9d19e9
Shterio <Shterio@users.noreply.github.com>
2019-07-13 00:40:18 +0200

    new

--- ASSOCIATIVE ARRAYS/03. Piccolo.js
+++ ASSOCIATIVE ARRAYS/03. Piccolo.js
@@ -2,3 +2,9 @@ picolo = (array) => {
     let line;
     let map = new Map();
 
+    for (let line of array) {
+        let [command, car] = line.split(', ');
+        if (command === 'IN') {
+            if (!map.has(car)) {
+                map.set(car, 1);
+            }

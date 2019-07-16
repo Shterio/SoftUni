b77f2ead0f1f6de549833fae6daff526cbebb74f
Shterio <Shterio@users.noreply.github.com>
2019-07-16 12:45:58 +0200

    new

--- TEXT PROCESSING/03. Extract File.js
+++ TEXT PROCESSING/03. Extract File.js
@@ -2,3 +2,10 @@ words = (text) => {
     let indexPoint = text.lastIndexOf('.');
     let index = text.lastIndexOf('\\');
 
+    let word = text.substring(index + 1, indexPoint);
+    let file = text.substr(indexPoint + 1)
+
+    console.log(`File name: ${word}`)
+    console.log(`File extension: ${file}`)
+};
+words('C:\\Internal\\training-internal\\Template.pptx');

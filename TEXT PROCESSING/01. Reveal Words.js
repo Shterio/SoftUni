a9136c31edb2b91fee84eb53f8c57362a1214956
Shterio <Shterio@users.noreply.github.com>
2019-07-16 12:44:13 +0200

    new

--- TEXT PROCESSING/01. Reveal Words.js
+++ TEXT PROCESSING/01. Reveal Words.js
@@ -1,3 +1,6 @@
 words =(searchWords, text) => {
     let searchWord = searchWords.split(', ');
 
+    searchWord.forEach(word => {
+        text = text.replace('*'.repeat(word.length), word)
+    });

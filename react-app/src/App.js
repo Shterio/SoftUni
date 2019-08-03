7261aae7bd0b12f1019615a5dcf40805440c6653
Shterio <Shterio@users.noreply.github.com>
2019-08-03 16:38:16 +0200

    new

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,20 +2,46 @@ import React from 'react';
 import './app.css';
 import './index.css'
 import ContactList from './contacts.json'
+import rerender from "./index";
 
-const makeContact = contact => (
-    <div className="contact" data-id="id">
+let contactIndex = 0;
+
+const makeContact = (data, currentIndex) => (
+    <div className="contact" key={data.email} data-id="id" onClick={e => selectDetailsContact(currentIndex, e)}>
         <span className="avatar small">&#9787;</span>
-        <span className="title">{contact.firstName} {contact.lastName}</span>
+        <span className="title">{data.firstName} {data.lastName}</span>
     </div>
 );
 
 const renderList = () => {
     const finalList = [];
     for (let contact of ContactList) {
-        finalList.push(makeContact(contact))
+        finalList.push(makeContact(contact, finalList.length))
     }
     return finalList;
+};
+
+const contactDetails = contact => (
+    <div className="content">
+        <div className="info">
+            <div className="col">
+                <span className="avatar">&#9787;</span>
+            </div>
+            <div className="col">
+                <span className="name">{contact.firstName}</span>
+                <span className="name">{contact.lastName}</span>
+            </div>
+        </div>
+        <div className="info">
+            <span className="info-line">&phone; {contact.phone}</span>
+            <span className="info-line">&#9993; {contact.email}</span>
+        </div>
+    </div>
+);
+
+const selectDetailsContact = index => {
+    contactIndex = index;
+    rerender(App(), document.getElementById('root'));
 };
 
 function App() {
@@ -27,34 +53,11 @@ function App() {
                     <h1>Contacts</h1>
                     <div className="content">
                         {renderList()}
-                        <div className="contact" data-id="id">
-                            <span className="avatar small">&#9787;</span>
-                            <span className="title">Jordan Kirov</span>
-                        </div>
-
-                        <div className="contact" data-id="id">
-                            <span className="avatar small">&#9787;</span>
-                            <span className="title">Maria Petrova</span>
-                        </div>
                     </div>
                 </div>
                 <div id="details">
                     <h1>Details</h1>
-                    <div className="content">
-                        <div className="info">
-                            <div className="col">
-                                <span className="avatar">&#9787;</span>
-                            </div>
-                            <div className="col">
-                                <span className="name">Ivan</span>
-                                <span className="name">Ivanov</span>
-                            </div>
-                        </div>
-                        <div className="info">
-                            <span className="info-line">&phone; 0887 123 456</span>
-                            <span className="info-line">&#9993; i.ivanov@gmail.com</span>
-                        </div>
-                    </div>
+                    {contactDetails(ContactList[contactIndex])}
                 </div>
             </div>
             <footer>Contact Book SPA &copy; 2017</footer>
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const rerender = ReactDOM.render;
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+export default rerender;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
